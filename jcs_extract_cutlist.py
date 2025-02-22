3eb18aa31fec7a99ae675befa8ca75aace077f20
Nick <53413353+nickpalladino@users.noreply.github.com>
2025-02-21 22:35:24 -0500

    Fixed page indexing and added manual overrides

--- jcs_extract_cutlist.py
+++ jcs_extract_cutlist.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from gmft.pdf_bindings import PyPDFium2Document
-from gmft.auto import CroppedTable, AutoTableDetector
+from gmft.auto import CroppedTable, AutoTableDetector, RotatedCroppedTable
 from gmft_pymupdf import PyMuPDFDocument
 from gmft.auto import AutoFormatConfig, AutoTableFormatter
 
@@ -10,7 +10,7 @@ def ingest_pdf(pdf_path, start_page, end_page) -> list[CroppedTable]:
     doc = PyMuPDFDocument(pdf_path)
 
     tables = []
-    for i in range(start_page, end_page):
+    for i in range(start_page-1, end_page):
         tables += detector.extract(doc.get_page(i))
     return tables, doc
         
@@ -65,9 +65,44 @@ config = AutoFormatConfig(verbosity=3, semantic_spanning_cells=True)
 formatter = AutoTableFormatter(config=config)
 tables, doc = ingest_pdf('HMOF_P_12X40_FullPlans.pdf', 10,15)
 
+# depending on rotation in the document some tables aren't extracted correctly automatically so
+# will override those ones afterwards
 for table in tables:
     ft = formatter.format(table)
     df = ft.df()
     df2 = merge_rows(df)
     df2.to_csv('page_' + str(ft.page.page_number+1) + '.csv', index=False)
 
+# 10 having issues detecting rows and columns, do by hand
+
+# 11 default is good
+
+# 12 needs rotation and col removal
+table12 = RotatedCroppedTable(page=tables[2].page, 
+                              bbox=tables[2].bbox, 
+                              confidence_score=tables[2].confidence_score, 
+                              label=tables[2].label, 
+                              angle=270)
+
+table12ft = formatter.format(table12)
+table12df = table12ft.df()
+table12df = table12df.dropna(axis=1, how='all')
+table12df.to_csv('page_12.csv', index=False)
+
+# 13 default is good
+
+# 14 needs rotation and col removal
+table14 = RotatedCroppedTable(page=tables[4].page, 
+                              bbox=tables[4].bbox, 
+                              confidence_score=tables[4].confidence_score, 
+                              label=tables[4].label, 
+                              angle=270)
+table14ft = formatter.format(table14)
+table14df = table14ft.df()
+# remove check column
+table14df = table14df.iloc[:, 1:]
+# merge cells
+table14dfmerge = merge_rows(table14df)
+table14df.to_csv('page_14.csv', index=False)
+
+# 15 default is good 
